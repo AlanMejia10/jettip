3c4315270fba6ba47955f0253d738fbc0b4f2ba5
Alan Mejia <aexme@pop-os.localdomain>
2022-10-21 03:38:07 -0500

    input field added

--- app/src/main/java/com/alanxmejia/jettip/MainActivity.kt
+++ app/src/main/java/com/alanxmejia/jettip/MainActivity.kt
@@ -3,22 +3,27 @@ package com.alanxmejia.jettip
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.PathNode
-import androidx.compose.ui.platform.textInputServiceFactory
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.alanxmejia.jettip.components.InputField
 import com.alanxmejia.jettip.ui.theme.JetTipTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +31,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                Text(text = "hello there")
+                //TopHeader()
+                MainContent()
             }
         }
     }
@@ -43,9 +49,9 @@ fun MyApp(content: @Composable () -> Unit) {
     }
 }
 
-@Preview
+//@Preview
 @Composable
-fun TopHeader() {
+fun TopHeader(totalPerPerson: Double = 100.0) {
     Surface(
         modifier = Modifier
             .fillMaxWidth()
@@ -58,9 +64,47 @@ fun TopHeader() {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            val total = "%.2f".format(totalPerPerson)
             Text(text = "Total per person", style = MaterialTheme.typography.h5)
             Spacer(modifier = Modifier.height(10.dp))
-            Text(text = "$100", style = MaterialTheme.typography.h4, fontWeight = FontWeight.Bold)
+            Text(text = "$$total", style = MaterialTheme.typography.h4, fontWeight = FontWeight.Bold)
+        }
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Preview
+@Composable
+fun MainContent(){
+    var totalBillState = remember {
+        mutableStateOf("")
+    }
+
+    val validState = remember(totalBillState.value) {
+        totalBillState.value.trim().isNotEmpty()
+    }
+
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    androidx.compose.material.Surface(
+        modifier = Modifier
+            .padding(2.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        border = BorderStroke(1.dp, Color.LightGray)
+    ) {
+        Column {
+            InputField(
+                valueState = totalBillState,
+                labelId = "Enter bill",
+                enabled = true,
+                isSingleLine = true,
+                onAction = KeyboardActions{
+                    if (!validState) return@KeyboardActions
+
+                    keyboardController?.hide()
+                }
+            )
         }
     }
 }
